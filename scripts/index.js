d7f914d6ceb43a1e3fa520bd70c1e61ba452407a
Michell Hernandez Guerra <mhg2705@hotmail.com>
2026-01-04 19:54:56 -0500

    Fix new post button selector and implement modal functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,24 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form")
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const modalForm = newPostModal.querySelector(".modal__form");
+const postLinkInput = newPostModal.querySelector("#post-link-input");
+const postCaptionInput = newPostModal.querySelector("#post-caption-input");
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function(){
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened")
 });
 
@@ -18,6 +30,24 @@ newPostBtn.addEventListener("click", function(){
   newPostModal.classList.add("modal_is-opened")
 });
 
+function handleNewPostSubmit(evt){
+  evt.preventDefault();
+  console.log(postLinkInput.value);
+  console.log(postCaptionInput.value);
+  newPostModal.classList.remove("modal_is-opened")
+};
+
+modalForm.addEventListener("submit", handleNewPostSubmit);
+
 newPostCloseBtn.addEventListener("click", function(){
   newPostModal.classList.remove("modal_is-opened")
 });
+
+function handleEditProfileSubmit(evt){
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
