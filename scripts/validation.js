69304e9a6c8160d7444cdd32b71bf86e8b643b51
Michell Hernandez Guerra <mhg2705@hotmail.com>
2026-05-02 10:48:29 -0400

    correction of validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -67,5 +67,16 @@ const enableValidation = (config) => {
   });
 };
 
+
+const settings = {
+  formSelector: '.modal__form',
+  inputSelector: '.modal__input',
+  submitButtonSelector: '.modal__button',
+  inactiveButtonClass: 'modal__button_disabled',
+  inputErrorClass: 'modal__input_type_error',
+  errorClass: 'modal__error_visible'
+};
+
+
 enableValidation(settings);
 
