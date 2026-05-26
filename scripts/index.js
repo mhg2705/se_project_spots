f5581d98028d55d114ab9de2383ae68d6428529a
Michell Hernandez Guerra <mhg2705@hotmail.com>
2026-05-26 14:37:45 -0400

    updated index.js

--- scripts/index.js
+++ scripts/index.js
@@ -29,14 +29,6 @@ const initialCards = [
   },
 ];
 
-const settings = {
-  formSelector: ".modal__form",
-  inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__button",
-  inactiveButtonClass: "modal__button_disabled",
-  inputErrorClass: "modal__input_type_error",
-  errorClass: "modal__error_visible"
-};
 
 //Profile elements
 const editProfileBtn = document.querySelector(".profile__edit-btn");
